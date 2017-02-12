f9ab609d76c2bbf0688bef957973459a3f85e0c3
eilincastillo <eilinalejandra@hotmail.com>
2017-02-12 19:01:18 -0400

    Método GET y POST para Specialty

--- src/AppBundle/Controller/SpecialtyController.php
+++ src/AppBundle/Controller/SpecialtyController.php
@@ -8,15 +8,34 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Specialty;
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations\Post;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\Get;
 
 
 class SpecialtyController extends FOSRestController
 {
     /**
-     * Create a job position
+     * Get all specialty
+     *
+     * @return mixed
+     *
+     * @Get("/")
+     */
+
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $specialty = $em->getRepository('AppBundle:Specialty')->findAll();
+        return $specialty;
+    }
+
+    /**
+     * Create a speciality
      * @var Request $request
      * @return mixed
      *
@@ -24,7 +43,41 @@ class SpecialtyController extends FOSRestController
      */
     public function createAction(Request $request)
     {
+        $content = $request->getContent();
 
+        if ($content != null)
+        {
+            $json = json_decode($content, true);
+            try
+            {
+                if ($json != null)
+                {
+                    $em = $this->getDoctrine()->getManager();
+
+                    $specialty = $em->getRepository('AppBundle:Specialty')->findByName($json["name"]);
+
+                    if ($specialty == null)
+                    {
+                        $specialty = new Specialty();
+                        $specialty->setName($json["name"]);
+
+                        $em->persist($specialty);
+                        $em->flush();
+                    }
+                    else
+                        return new Response('Error, the specialty already exists',Response::HTTP_CONFLICT);
+
+
+                    //return $specificationCriteria;
+                    return new Response('The specialty was successfully added', Response::HTTP_ACCEPTED);
+                }
+            }
+            catch (Exception $ex)
+            {
+                return new Response('Error, the specialty was not inserted',Response::HTTP_CONFLICT);
+            }
+        }
+        return new Response('Error, the specialty was not inserted',Response::HTTP_CONFLICT);
     }
 
 }
