d29609c78c84309080497bf5b06d96e150a499c9
Marco Franceschi <marcofranceschi@hotmail.com>
2017-03-26 15:09:41 -0400

    Navegacion con ui-router

--- web/app/config/routes.js
+++ web/app/config/routes.js
@@ -8,37 +8,42 @@ angular.module('cssi.routes').config( function ($stateProvider, $urlRouterProvid
             templateUrl: '../app/views/login/login.html',
             controller: 'LoginCtrl as ctrl'
         })
-        .state('doctor', {
+        .state('menu', {
+            url: '/menu',
+            abstract: true,
+            templateUrl: '../app/views/menu/menu.html'
+        })
+        .state('menu.doctor', {
             url: '/doctores/listado',
             templateUrl: '../app/views/doctor/list/doctor-list.html',
             controller: 'DoctorCtrl as ctrl'
         })
-        .state('doctor-add', {
+        .state('menu.doctor-add', {
             url: '/doctores/agregar',
             templateUrl: '../app/views/doctor/add/doctor-add.html',
             controller: 'DoctorCtrl as ctrl'
         })
-        .state('patient', {
+        .state('menu.patient', {
             url: '/pacientes/listado',
             templateUrl: '../app/views/patient/list/patient-list.html',
             controller: 'PatientCtrl as ctrl'
         })
-        .state('patient-add', {
+        .state('menu.patient-add', {
             url: '/pacientes/agregar',
             templateUrl: '../app/views/patient/add/patient-add.html',
             controller: 'PatientCtrl as ctrl'
         })
-        .state('user', {
+        .state('menu.user', {
             url: '/usuarios/listado',
             templateUrl: '../app/views/user/list/user-list.html',
             controller: 'UserCtrl as ctrl'
         })
-        .state('user-add', {
+        .state('menu.user-add', {
             url: '/usuarios/agregar',
             templateUrl: '../app/views/user/add/user-add.html',
             controller: 'UserCtrl as ctrl'
         })
-        .state('report', {
+        .state('menu.report', {
             url: '/reportes',
             templateUrl: '../app/views/report/report.html',
             controller: 'ReportCtrl as ctrl'
