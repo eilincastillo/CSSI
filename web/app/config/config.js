5aed515671e6be512cc74463f66138f73402d844
Marco Franceschi <marcofranceschi@hotmail.com>
2017-03-26 15:19:37 -0400

    Modificado gitignore

---
+++ web/app/config/config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('cssi.routes', []);
+angular.module('cssi.resources', []);
+angular.module('cssi.controllers.doctor', []);
+angular.module('cssi.controllers.login', []);
+angular.module('cssi.controllers.patient', []);
+angular.module('cssi.controllers.report', []);
+angular.module('cssi.controllers.user', []);
+angular.module('cssi.controllers.menu', []);
+angular.module('cssi.services.doctor', []);
+angular.module('cssi.services.login', []);
+angular.module('cssi.services.patient', []);
+angular.module('cssi.services.report', []);
+angular.module('cssi.services.user', []);
